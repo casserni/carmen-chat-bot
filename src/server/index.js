c32d8db5005d6c09d5a5d2b977f395aaa2bd0161
casserni@yahoo.com <nicholascassera@Nicholass-MBP.home>
2017-05-18 09:20:58 -0400

    initial commit

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,6 @@ const server = restify.createServer();
 
 server.post('/api/messages', connector.listen());
 
-server.listen(process.env.port, function () {
+server.listen(process.env.PORT, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
